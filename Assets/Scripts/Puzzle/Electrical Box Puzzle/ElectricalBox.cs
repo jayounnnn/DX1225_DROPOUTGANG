3e1560eb37b55723c0f9900a0fb768bd42a76410
Zekque <ongzhengbo.ozb@gmail.com>
2025-02-24 11:46:06 +0800

    Revert "Merge branch 'main' into Zheng-Bo"
    This reverts commit 8ff183191851685a97bce03324290c2247c02cb4, reversing
    changes made to f61ed6ce0a35090ce0da329f5c61abf43361f86a.

--- Assets/Scripts/Puzzle/Electrical Box Puzzle/ElectricalBox.cs
+++ Assets/Scripts/Puzzle/Electrical Box Puzzle/ElectricalBox.cs
@@ -1,64 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ElectricalBox : MonoBehaviour
+public class Ele : MonoBehaviour
 {
-    public GameObject minigameUIPanel;
-    private bool isPlayerNearby = false;
-
-    private void OnTriggerEnter(Collider other)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = true;
-            Debug.Log("Player is near the Electrical Box. Press E to interact.");
-        }
+        
     }
 
-    private void OnTriggerExit(Collider other)
+    // Update is called once per frame
+    void Update()
     {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-            Debug.Log("Player left the Electrical Box.");
-        }
-    }
-
-    private void Update()
-    {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
-        {
-            OpenMinigame();
-        }
-    }
-
-    public void OpenMinigame()
-    {
-        if (minigameUIPanel != null)
-        {
-            minigameUIPanel.SetActive(true);
-            Debug.Log("Minigame UI Opened.");
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Debug.LogWarning("Minigame UI Panel is not assigned in ElectricalBox!");
-        }
-    }
-
-    public void CloseMinigame()
-    {
-        if (minigameUIPanel != null)
-        {
-            minigameUIPanel.SetActive(false);
-            Debug.Log("Minigame UI Closed.");
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            Time.timeScale = 1f;
-        }
+        
     }
 }
